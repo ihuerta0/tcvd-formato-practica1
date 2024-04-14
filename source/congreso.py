4391d29c793e05daa5fdacc5002855f500544f94
ihuerta0 <165680858+ihuerta0@users.noreply.github.com>
2024-04-14 12:29:23 +0200

    Update congreso.py
    Afegida la part de descàrrega d'imatges, després de provar diferents formes, el més senzill és fer-ho a partir de l'enllaç de la imatge directament. 
    Queda pendent iterar entre totes les diputades (només varia un núm. en url de diputadx, i en imatge, i emmagatzemar les dades en csv.
    Un cop fet això, podem mirar si ens dona temps a fer lo de l'agenda.

--- source/congreso.py
+++ source/congreso.py
@@ -1,69 +1,70 @@
+import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from fake_useragent import UserAgent
-import requests
+
 
 # URL de la pàgina web
-url = 'https://www.congreso.es/es/busqueda-de-diputados'
+url = 'https://www.congreso.es/es/busqueda-de-diputados?p_p_id=diputadomodule&p_p_lifecycle=0&p_p_state=normal&p_p_mode=view&_diputadomodule_mostrarFicha=true&codParlamentario=1&idLegislatura=XV&mostrarAgenda=false'
 
-# Especifiquem capçalera
+# Especifica les capçaleres amb un User Agent aleatori
 ua = UserAgent()
-# Determinem un UserAgent aleatori
-print(ua.random)
 ua1 = ua.random
-
-# Especifico la resta de la capçalera
 headers = {'User-Agent': 'ua1',
            'Accept-Language': 'en-US,en;q=0.9,es;q=0.8',
            'Accept-Encoding': 'gzip, deflate, br',
            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
            'Referer': 'https://www.google.com'}
 
-# Creem la sol·licitud amb les capçaleres especificades
+# Crea la sol·licitud amb les capçaleres especificades
 response = requests.get(url, headers=headers)
+status_code = response.status_code  # Captura el codi d'estat
 
-# Capturar el codi d'estat de la resposta HTTP
-status_code = response.status_code
-
-# Comprovar si la sol·licitud ha estat satisfactòria (codi d'estat 200)
+# Comprova si la sol·licitud ha estat satisfactòria (codi d'estat 200)
 if status_code != 200:
     print(f"No s'ha pogut obtenir el codi font de la pàgina web. Codi d'estat: {status_code}")
 else:
-    # Configurem Selenium
-    # Utilitza la ruta
+    # Configura Selenium
     firefox_path = '/snap/bin/geckodriver'
-    
-    # Configurem el servei del driver de Firefox amb la ruta de l'executable
     firefox_service = Service(firefox_path)
-
-    # Inicialitzem el driver de Firefox amb el servei configurat
     driver = webdriver.Firefox(service=firefox_service)
 
-    # Naveguem a la pàgina web
+    # Navega a la pàgina web
     driver.get(url)
-
-    # Esperem a que s'executi el JavaScript
     driver.implicitly_wait(10)
 
-    # Obtenim el contingut de la pàgina després de l'execució de JavaScript
+    # Obte el contingut de la pàgina després de l'execució de JavaScript
     page_content = driver.page_source
 
-    # Creem un objecte BeautifulSoup amb el contingut de la página
+    # Crea un objecte BeautifulSoup amb el contingut de la pàgina
     soup = BeautifulSoup(page_content, 'html.parser')
+
+    # URL de la imatge
+    url_image = 'https://www.congreso.es/docu/imgweb/diputados/1_15.jpg'
+
+    # Crea la sol·licitud amb les capçaleres especificades
+    response = requests.get(url_image, headers=headers)
+    status_code = response.status_code  # Captura el codi d'estat
+
+    # Comprova si la sol·licitud ha estat satisfactòria (codi d'estat 200)
+    if status_code != 200:
+        print(f"No s'ha pogut obtenir el codi font de la pàgina web\
+            d'obtenció de la imatge. Codi d'estat: {status_code}")
+    else:
+        # Descarrega la imatge
+        with open('image.jpg', 'wb') as file:
+            file.write(response.content)
+        print("Imatge descarregada correctament com a 'image.jpg'")
 
     # Prettify the HTML content
     pretty_page_content = soup.prettify()
 
-    # Imprimim el codi font de la pàgina web formatejat
-    # print(pretty_page_content)
-
-    # Tanquem el navegador
+    # Tanca el navegador
     driver.quit()
 
-    # Escrivim el contingut de la pàgina a un arxiu .txt
+    # Escribe el contingut de la pàgina a un arxiu .txt
     with open('page_content.txt', 'w', encoding='utf-8') as file:
         file.write(pretty_page_content)
 
     print("Contingut de la pàgina guardat correctament en page_content.txt")
-
